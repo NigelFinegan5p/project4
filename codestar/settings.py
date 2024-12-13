a6caafa5e931b93c5a26bbf9f61c7919579462cc
Nigel Finegan <fvunpiqh@students.codeinstitute.net>
2024-12-13 12:06:40 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0v5pi**z78o&tuynfeh_7xkswzc*nnq27%!9kgyifk(^@4g$l$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-nigelfinegan5p-project4-oqdp1m87j1l.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
