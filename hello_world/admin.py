5290e5088b5d01ad2ee55ca989b81f2fac017f8e
Nigel Finegan <fvunpiqh@students.codeinstitute.net>
2025-01-08 18:34:49 +0000

    add admin.py file admin class

--- hello_world/admin.py
+++ hello_world/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import GiftBox, Booking
 
-# Register your models here.
+# Define a custom admin class for the GiftBox model
+class GiftBoxAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    search_fields = ('name', 'description')
+    list_filter = ('name',)
+    ordering = ('name',)
+
+# Define a custom admin class for the Booking model
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'giftbox', 'customer_email', 'date_booked')
+    search_fields = ('customer_name', 'customer_email')
+    list_filter = ('giftbox', 'date_booked')
+    ordering = ('-date_booked',)
+
+# Register the models with the admin site
+admin.site.register(GiftBox, GiftBoxAdmin)
+admin.site.register(Booking, BookingAdmin)
+
