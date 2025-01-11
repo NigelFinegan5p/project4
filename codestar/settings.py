95978896b6de56762dd03acc2b61c5dd85e0cd04
Nigel Finegan <fvunpiqh@students.codeinstitute.net>
2025-01-11 18:10:10 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-nigelfinegan5p-project4-ac4x03vi2g0.ws.codeinstitute-ide.net','.herokuapp.com']
 # '8000-nigelfinegan5p-project4-ac4x03vi2g0.ws.codeinstitute-ide.net'
 # '8000-nigelfinegan5p-project4-oqdp1m87j1l.ws.codeinstitute-ide.net'
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'hello_world',
     'firstapp',
     'django_summernote',
+    'cloudinary',
     'blog',
     'gifts',
 ]
