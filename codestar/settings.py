7b3c0dfd0f917d4ef6e6925a18ad9cd5f84ab67b
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-05-06 16:41:20 +0100

    debug false and updated settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['8000-nigelfinegan5p-project4-ac4x03vi2g0.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 # Application definition
 
