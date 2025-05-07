7c22d8e194cb73f0df872c6c9c2792ce347643d6
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-05-07 09:29:28 +0100

    settings update static settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -163,8 +163,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# Production settings when DEBUG = False
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
